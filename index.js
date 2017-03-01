a4021b9bf926a0dcb9f57a5c6279922e32f086de
El Hector <a291528@uach.mx>
2017-02-28 17:02:28 -0700

    se agrega la calculadora

---
+++ index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+
+app.get('/', (req, res) => {
+
+    res.send('Mande una expresion en la url');
+    res.end();
+});
+
+app.get('/:id', (req, res) => {
+  var expresion = req.params.id;
+
+  try {
+    result = eval(expresion);
+  } catch(error) {
+    result = error;
+  }
+
+  res.send(result.toString());
+  res.end();
+})
+
+app.listen(process.env.PORT || '3000');
